b0de69d0277d9c3d92d8cb97607df99d2ea89932
Aaron <aarongkendall@gmail.com>
2020-03-29 12:25:08 +0100

    added some more responses

--- src/responses.js
+++ src/responses.js
@@ -5,4 +5,8 @@ export default [
     "I say NO to malarkey!",
     "I am drawing a line in the sand - the malarkey stops here!",
     "I didn't stand for malarkey with Corn Pop and I certainly will not stand for it now",
+    "listen Jack, I'm going to set you straight - stop with the malarkey or I'll give you a taste of what I gave to ol' Corn Pop!",
+    "if my time as Obama's Secretary of State from 1983 - 2004 taught me anything it's that malarkey solves nothing. Quit with it, son",
+    "now usually if you're saying it then you can take that to the bank, but bucko, stop spreading malarkey",
+    "now I'll say this nice one time - stop the malarkey. Don't make me come back here"
 ]
